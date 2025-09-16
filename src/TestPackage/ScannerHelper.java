ef6623ee3117bffba2e83cf1356c98f0af6d7968
lindkvst <lindkviste@gmail.com>
2025-09-16 15:48:28 +0200

    Scannr

--- src/TestPackage/ScannerHelper.java
+++ src/TestPackage/ScannerHelper.java
@@ -1,4 +1,11 @@
 package TestPackage;
 
+import java.util.Scanner;
+
 public class ScannerHelper {
+    private Scanner sc;
+
+    public ScannerHelper() {
+        this.sc = new Scanner(System.in);
+    }
 }
