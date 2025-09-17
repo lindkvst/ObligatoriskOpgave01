c3a258d07b6452983ff1af6273e9d03554a4eae8
lindkvst <lindkviste@gmail.com>
2025-09-17 09:35:01 +0200

    Scanner update

--- src/TestPackage/ScannerHelper.java
+++ src/TestPackage/ScannerHelper.java
@@ -12,13 +12,22 @@ public class ScannerHelper {
     //En hilsen til brugeren, der fortæller hvad der skal ske.
     public void welcomeMessage() {
         System.out.println("Welcome to the Loan Store. We're thrilled to have you here");
-        System.out.println("Let's get ready to register some loans");
+        System.out.println("Let's get ready to register some loans! \n");
     }
 
     public int  numOfLoans() {
         boolean numCorrect = false;
         int numOfLoans = 0;
 
-        while (!)
+        while (!numCorrect) {
+
+            System.out.println("How many loans do you want to register today?");
+            //nedenstående er input validering, der sikrer at vi registrerer en int inden for et
+            //accepteret range, som ikke er et negativt tal.
+            if (sc.hasNextInt()) { //scanner
+                numOfLoans = sc.nextInt();
+                sc.nextLine(); // rydder enter fra input bufferen, da dette kan give problemer
+            }
+        }
     }
 }
