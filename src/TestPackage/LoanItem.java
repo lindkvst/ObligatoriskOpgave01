f7f127078e3af4e3c411cd3c035c345a524bc462
Muhammedsarac-debug <muhammed.sarac@outlook.dk>
2025-09-17 08:27:42 +0100

    .

--- src/TestPackage/LoanItem.java
+++ src/TestPackage/LoanItem.java
@@ -1,4 +1,30 @@
 package TestPackage;
 
 public class LoanItem {
+    protected String type;
+    protected String name;
+
+    public LoanItem(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanItem{" + "type=" + type + ", name=" + name + '}';
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
