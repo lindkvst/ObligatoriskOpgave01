d08be646a874c5a94419f1df8f7934af7179381d
MartinWagnerThomsen <marwagtho@gmail.com>
2025-09-17 09:57:57 +0200

    Commit video 01

--- src/TestPackage/Video.java
+++ src/TestPackage/Video.java
@@ -1,5 +1,35 @@
 package TestPackage;
 
-public class Video {
+public class Video extends LoanItem{
+
+    public Video(String type, String name) {
+        super(type, name);
+        this.type = type;
+        this.name = name;
+
+    }
+    //getter
+    public String getType() {
+        return type;
+    }
+    //setter
+    public void setType(String type) {
+        this.type = type;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //Vi overrider toString metoden
+    @Override
+    public String toString() {
+        return "Video{" + "type=" + type + ", name=" + name + '}';
+    }
+    public void
 }
-//wassup
