004d35f2216d517568077ec8346a9a3b52cda01e
trist <trho0001@stud.ek.dk>
2025-09-17 09:58:41 +0200

    Test class

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -1,4 +1,39 @@
 package TestPackage;
 
 public class TestClass {
+    public static void main(String[] args){
+
+        String loanType;
+
+        String loanName;
+
+        ScannerHelper Sc = new ScannerHelper();
+        Sc.welcomeMessage();
+
+
+        int loanCollection = Sc.numOfLoans();
+
+        LoanItem[] loanCollection = new LoanItem[];
+
+        for (int i = 0); i < loanCollection.length; i++){
+            int regCount = i + 1;
+            System.out.println("\nRegister loan: " + regCount);
+            loanType = Sc.askType();
+            loanName = Sc.askName();
+
+            if (loanType.equalsIgnoreCase("video")) ;
+            //video
+            loanCollection[i] = new Video(loanName);
+
+        }else if (loanType.equalsIgnoreCase("book")){
+         //book
+        }
+    }
+    // Loan registration
+    for (int i = 0; i < loanCollection.lenght; i++){
+        System.out.println(loanCollection[i]);
+    }
+
+
+
 }
